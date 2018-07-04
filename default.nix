f17ed53ae03cdbc7a49c4689cec3ca025642f60e
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-07-03 18:20:51 -0700

    Refactor Nix code
    This pins `nixpkgs` and uses `readDirectory` to simplify testing dependency
    changes

--- default.nix
+++ default.nix
@@ -1,14 +1,29 @@
-{ mkDerivation, async, base, contravariant, pipes, semigroups
-, stdenv, stm, void
-}:
-mkDerivation {
-  pname = "pipes-concurrency";
-  version = "2.0.10";
-  src = ./.;
-  libraryHaskellDepends = [
-    async base contravariant pipes semigroups stm void
-  ];
-  testHaskellDepends = [ async base pipes stm ];
-  description = "Concurrency for the pipes ecosystem";
-  license = stdenv.lib.licenses.bsd3;
-}
+let
+  fetchNixpkgs = import ./nix/fetchNixpkgs.nix;
+
+  nixpkgs = fetchNixpkgs {
+    rev = "804060ff9a79ceb0925fe9ef79ddbf564a225d47";
+
+    sha256 = "01pb6p07xawi60kshsxxq1bzn8a0y4s5jjqvhkwps4f5xjmmwav3";
+
+    outputSha256 = "0ga345hgw6v2kzyhvf5kw96hf60mx5pbd9c4qj5q4nan4lr7nkxn";
+  };
+
+  readDirectory = import ./nix/readDirectory.nix;
+
+  config = {
+    packageOverrides = pkgs: {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = readDirectory ./nix;
+      };
+    };
+  };
+
+  pkgs =
+    import nixpkgs { inherit config; };
+
+in
+  { inherit (pkgs.haskellPackages) pipes-concurrency;
+
+    shell = (pkgs.haskell.lib.doBenchmark pkgs.haskellPackages.pipes-concurrency).env;
+  }
