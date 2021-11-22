09e6ce6aa1776c35dcc9dad029832f276c4ae165
Gabriella Gonzalez <Gabriel439@gmail.com>
2021-11-22 14:35:44 -0800

    Remove semigroups dependency (#58)
    Related to https://github.com/commercialhaskell/stackage/issues/6308

--- default.nix
+++ default.nix
@@ -1,22 +1,22 @@
 let
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOs/nixpkgs/archive/312a059bef8b29b4db4e73dc02ff441cab7bb26d.tar.gz";
+    url = "https://github.com/NixOs/nixpkgs/archive/09650059d7f5ae59a7f0fb2dd3bfc6d2042a74de.tar.gz";
 
-    sha256 = "1j52yvkhw1inp6ilpqy81xv1bbwgwqjn0v9647whampkqgn6dxhk";
+    sha256 = "0f06zcc8mh934fya0hwzklmga238yxiyfp183y48vyzkdsg7xgn0";
   };
 
-  readDirectory = import ./nix/readDirectory.nix;
-
-  config = {
-    packageOverrides = pkgs: {
-      haskellPackages = pkgs.haskellPackages.override {
-        overrides = readDirectory ./nix;
-      };
-    };
+  overlay= pkgsNew: pkgsOld: {
+    haskellPackages = pkgsOld.haskellPackages.override (old: {
+      overrides =
+        pkgsNew.lib.composeExtensions
+          (old.overrides or (_: _: { }))
+          (pkgsNew.haskell.lib.packageSourceOverrides {
+            pipes-concurrency = ./.;
+          });
+    });
   };
 
-  pkgs =
-    import nixpkgs { inherit config; };
+  pkgs = import nixpkgs { config = { }; overlays = [ overlay ]; };
 
 in
   { inherit (pkgs.haskellPackages) pipes-concurrency;
